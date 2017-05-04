1b08d22995def82fe1d3dcb5ba4cf8ced342cbd5
Manuel Mauky <manuel.mauky@saxsys.de>
2017-05-04 18:09:36 +0200

    Hide rest resources for user and shoppingCart

--- shop-backend/src/main/java/eu/lestard/shopexample/backend/data/ShoppingCartRepository.java
+++ shop-backend/src/main/java/eu/lestard/shopexample/backend/data/ShoppingCartRepository.java
@@ -1,6 +1,8 @@
 package eu.lestard.shopexample.backend.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface ShoppingCartRepository extends CrudRepository<ShoppingCart, String> {
 }
