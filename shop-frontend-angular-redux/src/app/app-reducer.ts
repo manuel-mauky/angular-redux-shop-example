53a883ba25c5a2730c3dddf91f78ae33aa8f17a0
Manuel Mauky <manuel.mauky@googlemail.com>
2017-05-10 22:47:33 +0200

    enable routing in redux

--- shop-frontend-angular-redux/src/app/app-reducer.ts
+++ shop-frontend-angular-redux/src/app/app-reducer.ts
@@ -3,6 +3,7 @@ import { AppState } from './appstate.types';
 import { productsInitialState, productsReducer } from './products/redux/reducer';
 import { shoppingCartInitialState, shoppingCartReducer } from './shopping-cart/redux/reducer';
 import { Reducer, combineReducers } from 'redux';
+import { routerReducer } from "@angular-redux/router";
 
 
 export const initialState: AppState = {
@@ -13,4 +14,5 @@ export const initialState: AppState = {
 export const appReducer: Reducer<AppState> = combineReducers<AppState>({
     products: productsReducer,
     shoppingCart: shoppingCartReducer,
+    router: routerReducer,
 })
